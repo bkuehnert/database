f0906b47e0584c457e8c1c271e624a63b73de26c
Josh <jsobel29@gmail.com>
2018-11-07 23:14:46 -0500

    Set

---
+++ Set.h
@@ -0,0 +1,65 @@
+typedef struct LongLinkedListSetNode
+{
+	long tag;
+	struct LongLinkedListSetNode* next;
+} Node;
+
+typedef struct LongLinkedListSet
+{
+	struct LongLinkedListSetNode* first;
+	struct LongLinkedListSetNode* last;
+	int size;
+} LongLinkedListSet;
+
+void add(LongLinkedListSet* list, long add)
+{
+	if(list->size == 0)
+	{
+		Node* toAdd = (Node*)calloc(1, sizeof(Node));
+		toAdd->tag = add;
+		list->first = toAdd;
+		list->last = toAdd;
+		list->size = 1;
+	}
+	else
+	{
+		for(Node* check = list->first; check!=0; check=check->next)
+		{
+			if(check->tag == add)
+				return;
+		}
+
+		Node* toAdd = (Node*)calloc(1, sizeof(Node));
+		toAdd->tag = add;
+		list->last->next = toAdd;
+		list->last = toAdd;
+		list->size++;
+	}
+}
+
+int indexOf(LongLinkedListSet* list, long checkLong)
+{
+	int i = 0;
+
+	for(Node* check = list->first; check!=0; check=check->next)
+	{
+		if(check->tag == checkLong)
+			return i;
+
+		i++;
+	}
+
+	return -1;
+}
+
+void freeList(LongLinkedListSet* list)
+{
+	Node* cur = list->first;
+
+	while(cur)
+	{
+		Node* next = cur->next;
+		free(cur);
+		cur = next;
+	}
+}
