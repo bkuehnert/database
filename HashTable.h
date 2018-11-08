9ab28fd28274a057e9f129ccbaf77ed2d04ba407
Ben Kuehnert <bkuehnert@gmail.com>
2018-11-07 23:19:00 -0500

    keyvector, some functions

--- HashTable.h
+++ HashTable.h
@@ -2,6 +2,7 @@
 #define _h_hashset
 
 #include "Tuple.h"
+#include "Set.h"
 
 typedef struct Node 
 {
@@ -12,9 +13,14 @@ typedef struct Node
 typedef struct HashTable 
 {
 	struct Node** buckets;		
+	int* key_vector;
 	int size;
 	int count;
 } HashTable;
+
+void ht_put(Tuple t);
+
+Set ht_get(Tuple t);
 
 
 #endif
