b17123625b3dd62f9049e6af78f9e6519bd12cdf
Ben Kuehnert <bkuehnert@gmail.com>
2018-11-11 21:54:27 -0500

    write insert

--- HashTable.c
+++ HashTable.c
@@ -26,7 +26,25 @@ HashTable* create_HashTable(int* key_vector)
 {
 	HashTable* this = (HashTable*)malloc(sizeof(struct HashTable));
 	this->buckets = (ht_Node**)malloc(1009*sizeof(ht_Node*));
+	for(int i = 0; i < 1009; i++)
+		this->buckets[i] = NULL;
 	this->key_vector = key_vector;
 	this->count = 0;
 	return this;
 }
+
+ht_Node* create_htNode(Tuple* data) {
+	ht_Node* this = (ht_Node*)malloc(sizeof(struct ht_Node));
+	this->data = data;
+	this->next = NULL;
+	return this;
+}
+
+void ht_put(HashTable* h, Tuple* t)
+{
+	ht_Node* bucket = h->buckets[hash(h->key_vector, t)];
+	for(ht_Node* ptr = bucket; ptr != NULL; ptr = ptr->next)
+		if(ptr->next == NULL) ptr->next = create_htNode(t);
+	h->count++;
+}
+
--- HashTable.h
+++ HashTable.h
@@ -19,10 +19,10 @@ typedef struct HashTable
 
 HashTable* create_HashTable(int* key_vector);
 
-void ht_put(Tuple t);
+void ht_put(HashTable* h, Tuple* t);
 
 //should make this a hashmap itself
-Set ht_get(Tuple t);
+Set* ht_get(Tuple* t);
 
 
 #endif
