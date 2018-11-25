58dc576ecb262844fd9b69e8222508de203cbb9a
Josh <jsobel29@gmail.com>
2018-11-25 00:40:26 -0500

    plugged glitch

--- Examples.h
+++ Examples.h
@@ -3,6 +3,7 @@
 void whatGrade(char* studentname, char* coursename, Relation* snap, Relation* csg)
 {
 	int columns[] = {1,1,0,0};
-	Relation* joined = joinRelation(project(snap, columns), csg, 0, 1);
+	Relation* newRel = project(snap, columns);
+	Relation* joined = joinRelation(newRel, csg, 0, 1);
 	print(rel_query(joined, createTuple(4, "*", studentname, coursename, "*")));
 }
